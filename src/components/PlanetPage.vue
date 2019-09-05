0251a17f0e963509f0d4aeef3d19cb37fd2c6067
Thomas NeFFeX <jakediego01@gmail.com>
2019-09-05 13:19:59 +0300

    refactored components

--- src/components/PlanetPage.vue
+++ src/components/PlanetPage.vue
@@ -10,15 +10,15 @@
 import * as THREE from 'three'
 import * as OrbitControls from 'three-orbitcontrols'
 import store from '../store'
-import sunTexture from "../2k_sun.jpg"
-import marsTexture from "../2k_mars.jpg"
-import jupiterTexture from "../2k_jupiter.jpg"
-import mercuryTexture from "../2k_mercury.jpg"
-import venusTexture from "../2k_venus.jpg"
-import earthTexture from "../2k_earth.jpg"
-import saturnTexture from "../2k_saturn.jpg"
-import uranusTexture from "../2k_uranus.jpg"
-import neptuneTexture from "../2k_neptune.jpg"
+import sunTexture from "../assets/textures/sun.jpg"
+import marsTexture from "../assets/textures/mars.jpg"
+import jupiterTexture from "../assets/textures/jupiter.jpg"
+import mercuryTexture from "../assets/textures/mercury.jpg"
+import venusTexture from "../assets/textures/venus.jpg"
+import earthTexture from "../assets/textures/earth.jpg"
+import saturnTexture from "../assets/textures/saturn.jpg"
+import uranusTexture from "../assets/textures/uranus.jpg"
+import neptuneTexture from "../assets/textures/neptune.jpg"
 
 
 export default {
@@ -32,70 +32,70 @@ export default {
 		}
 	},
 	mounted() {
-		var camera, scene, renderer, controls;
-var planets = [];
-var timestamp = 0;
-var scaleVector = new THREE.Vector3();
+		let camera, scene, renderer, controls;
+const planets = [];
+let timestamp = 0;
+const scaleVector = new THREE.Vector3();
 
-init();
-animate();
 
-function init() {
+
+const init = () => {
+
+	let width = window.innerWidth;
+	let height = window.innerHeight;
 
   scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
+  camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
   camera.position.set(0, 100, 175);
 
   renderer = new THREE.WebGLRenderer({
     antialias: true
   });
-  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setSize(width, height);
   document.body.appendChild(renderer.domElement);
 
   controls = new OrbitControls(camera, renderer.domElement);
 
 
-  var createPlanet = function(name, radius, orbit, speed, texture) {
-    var geom = new THREE.SphereGeometry(radius, 32, 16);
-    var mat = new THREE.MeshBasicMaterial({
-      //color: Math.random() * 0xFFFFFF,
+  const createPlanet = (name, radius, orbit, speed, texture) => {
+    const geom = new THREE.SphereGeometry(radius, 32, 16);
+    const mat = new THREE.MeshBasicMaterial({
 	  map: new THREE.TextureLoader().load(texture)
-      //wireframe: true
     });
-    var planet = new THREE.Mesh(geom, mat);
+    const planet = new THREE.Mesh(geom, mat);
     planet.userData.orbit = orbit;
     planet.userData.speed = speed;
 
-    var canvas = document.createElement('canvas');
+    const canvas = document.createElement('canvas');
     canvas.width = 256;
     canvas.height = 256;
-    var ctx = canvas.getContext("2d");
+    const ctx = canvas.getContext("2d");
     ctx.font = "44pt Arial";
     ctx.fillStyle = "white";
     ctx.textAlign = "center";
     ctx.fillText(name, 128, 44);
-    //console.log(ctx);
-    var tex = new THREE.Texture(canvas);
+
+    const tex = new THREE.Texture(canvas);
     tex.needsUpdate = true;
-    var spriteMat = new THREE.SpriteMaterial({
+    const spriteMat = new THREE.SpriteMaterial({
       map: tex
     });
-    var sprite = new THREE.Sprite(spriteMat);
+    const sprite = new THREE.Sprite(spriteMat);
 
     planet.add(sprite);
     planets.push(planet);
     scene.add(planet);
 
     //orbit
-    var shape = new THREE.Shape();
+    const shape = new THREE.Shape();
     shape.moveTo(orbit, 0);
     shape.absarc(0, 0, orbit, 0, 2 * Math.PI, false);
-    var spacedPoints = shape.createSpacedPointsGeometry(128);
+    const spacedPoints = shape.createSpacedPointsGeometry(128);
     spacedPoints.rotateX(THREE.Math.degToRad(-90));
-    var orbit = new THREE.Line(spacedPoints, new THREE.LineBasicMaterial({
+    const orbitLine = new THREE.Line(spacedPoints, new THREE.LineBasicMaterial({
       color: "white"
     }));
-    scene.add(orbit);
+    scene.add(orbitLine);
   };
 
   createPlanet("Mercury", 1, 10, 5, mercuryTexture);
@@ -107,34 +107,36 @@ function init() {
   createPlanet("Uranus", 1.75, 80, 0.4, uranusTexture);
   createPlanet("Neptune", 0.8, 90, 0.2, neptuneTexture);
 
-  //console.log(planets[0].children[0]);
 
-  var sun = new THREE.Mesh(new THREE.SphereGeometry(5, 32, 16), new THREE.MeshBasicMaterial({
+  const sun = new THREE.Mesh(new THREE.SphereGeometry(5, 32, 16), new THREE.MeshBasicMaterial({
     map: new THREE.TextureLoader().load(sunTexture)
   }));
   scene.add(sun);
 }
 
-function animate() {
+const render = () => {
+  renderer.render(scene, camera);
+}
+
+const animate = () => {
   timestamp = Date.now() * 0.0001;
   requestAnimationFrame(animate);
   planets.forEach(function(planet) {
     
-    var scaleFactor = 8;
-    var sprite = planet.children[0];
-    var scale = scaleVector.subVectors(planet.position, camera.position).length() / scaleFactor;
+    const scaleFactor = 8;
+    const sprite = planet.children[0];
+    const scale = scaleVector.subVectors(planet.position, camera.position).length() / scaleFactor;
     sprite.scale.set(scale, scale, 1);
-    var orbit = planet.userData.orbit;
-    var speed = planet.userData.speed;
+    const orbit = planet.userData.orbit;
+    const speed = planet.userData.speed;
     planet.position.x = Math.cos(timestamp * speed) * orbit;
     planet.position.z = Math.sin(timestamp * speed) * orbit;
   });
   render();
 }
 
-function render() {
-  renderer.render(scene, camera);
-}
+init();
+animate();
 
 	}
 }	
